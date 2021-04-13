887910513770fdfea7890c4574e31ba6e03c1032
Letitia Robinson <letitiarobinson@Letitias-MacBook-Pro.local>
2021-04-13 05:54:38 -0400

    get back up

---
+++ robinson.tish/js/function.js
@@ -0,0 +1,15 @@
+
+// Promise
+const query = (options) => {
+   return fetch('data/api.php',{
+      method:'POST',
+      body:JSON.stringify(options)
+   }).then(d=>d.json());
+}
+
+
+// Curried function(a function that returns a function)
+const templater = f => a =>
+	(Array.isArray(a)?a:[a])
+   .reduce((r,o,i,a)=>r+f(o,i,a),'');
+
